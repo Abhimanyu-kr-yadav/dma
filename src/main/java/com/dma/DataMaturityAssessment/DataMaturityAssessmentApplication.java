87d2b491c484954381390eb8a126dab676b5c6c5
abhimanyu <abhimanyu6280@gmail.com>
2022-07-01 13:23:21 +0530

    First commit - Abhimanyu

---
+++ src/main/java/com/dma/DataMaturityAssessment/DataMaturityAssessmentApplication.java
@@ -0,0 +1,13 @@
+package com.dma.DataMaturityAssessment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataMaturityAssessmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataMaturityAssessmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dma/DataMaturityAssessment/DataMaturityAssessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dma.DataMaturityAssessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataMaturityAssessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
