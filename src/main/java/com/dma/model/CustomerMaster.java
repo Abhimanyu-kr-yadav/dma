0d883c0350c2eb7046c89a59c940815d66b2a9a9
abhimanyu <abhimanyu6280@gmail.com>
2022-07-01 14:02:37 +0530

    Created data memebers in 3 model classes

--- src/main/java/com/dma/model/CustomerMaster.java
+++ src/main/java/com/dma/model/CustomerMaster.java
@@ -1,5 +1,26 @@
 package com.dma.model;
 
-public class CustomerMaster {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "customer_master")
+public class CustomerMaster {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long customer_id;
+	
+	@Column(name = "customer_name")
+	private String customer_name;
 }
--- src/main/java/com/dma/model/QuestionMaster.java
+++ src/main/java/com/dma/model/QuestionMaster.java
@@ -1,5 +1,29 @@
 package com.dma.model;
 
-public class QuestionMaster {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "question_master")
+public class QuestionMaster {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long question_id;
+	
+	@Column(name = "question_name")
+	private String question_name;
+	
+	
 }
